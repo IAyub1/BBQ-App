0c6b0c3324bb3b039bfb2f5651d17cdffed8e549
IAyub1 <ismaelayub786@hotmail.com>
2025-03-27 14:39:37 +0000

    removed try catch block

--- index.js
+++ index.js
@@ -7,29 +7,23 @@ document.getElementById("forecast-button").addEventListener("click", async () =>
         return;
     }
 
-    try {
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&units=metric&cnt=5&appid=${API_KEY}`);
-        const data = await response.json();
 
-        const weatherData = {
-            day1: new Date(1000 * data.list[0].dt).toLocaleDateString('en-us', { weekday: 'short' }),
-            day1Weather: data.list[0].temp,
-            day2: new Date(1000 * data.list[1].dt).toLocaleDateString('en-us', { weekday: 'short' }),
-            day2Weather: data.list[1].temp,
-            day3: new Date(1000 * data.list[2].dt).toLocaleDateString('en-us', { weekday: 'short' }),
-            day3Weather: data.list[2].temp,
-            day4: new Date(1000 * data.list[3].dt).toLocaleDateString('en-us', { weekday: 'short' }),
-            day4Weather: data.list[3].temp,
-            day5: new Date(1000 * data.list[4].dt).toLocaleDateString('en-us', { weekday: 'short' }),
-            day5Weather: data.list[4].temp
-        };
-
-        localStorage.setItem("weatherData", JSON.stringify(weatherData));
-        window.location.href = "forecast.html";
-    } catch (error) {
-        console.error(error);
-        alert("Failed to fetch weather. Please check the city name.");
-    }
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&units=metric&cnt=5&appid=${API_KEY}`);
+    const data = await response.json();
+    const weatherData = {
+        day1: new Date(1000 * data.list[0].dt).toLocaleDateString('en-us', { weekday: 'short' }),
+        day1Weather: data.list[0].temp,
+        day2: new Date(1000 * data.list[1].dt).toLocaleDateString('en-us', { weekday: 'short' }),
+        day2Weather: data.list[1].temp,
+        day3: new Date(1000 * data.list[2].dt).toLocaleDateString('en-us', { weekday: 'short' }),
+        day3Weather: data.list[2].temp,
+        day4: new Date(1000 * data.list[3].dt).toLocaleDateString('en-us', { weekday: 'short' }),
+        day4Weather: data.list[3].temp,
+        day5: new Date(1000 * data.list[4].dt).toLocaleDateString('en-us', { weekday: 'short' }),
+        day5Weather: data.list[4].temp
+    };
+    localStorage.setItem("weatherData", JSON.stringify(weatherData));
+    window.location.href = "forecast.html";
 });
 
 document.getElementById("timer-button").addEventListener("click", () => {
