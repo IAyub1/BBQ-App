6e714fdd7b05cea4fe1191034266ba8ab4235f80
IAyub1 <ismaelayub786@hotmail.com>
2025-04-03 17:05:35 +0100

    hello

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-document.getElementById("forecast-button").addEventListener("click", async () => {
+const city = 'London';
+const API_KEY = '5ef53af545dd04589b5c843faae7edef';
 
-    const city = document.getElementById("location-input").value;
-    const API_KEY = '5ef53af545dd04589b5c843faae7edef';
-
-    if (!city) {
-        alert("Please enter a city.");
-        return;}
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&units=metric&cnt=5&appid=${API_KEY}`);
-        if (!response.ok) throw new Error("City not found");
-        const data = await response.json();
-
-        const weatherData = {
-            day1: new Date(1000 * data.list[0].dt).toLocaleDateString('en-us', { weekday: 'short' }),
-            day1Weather: data.list[0].temp}
-    
-    localStorage.setItem("weatherData", JSON.stringify(weatherData));
-    window.location.href = "forecast.html";
-})
+   
+const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&units=metric&cnt=5&appid=${API_KEY}`);
+if (!response.ok) throw new Error("City not found");
+const data = await response.json();
+const weatherData = {
+	day1: new Date(1000 * data.list[0].dt).toLocaleDateString('en-us', { weekday: 'short' }),
+	day1Weather: data.list[0].temp
+};
