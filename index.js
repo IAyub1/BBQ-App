5a921224c2e4dfcfc954d8f2c2ff779e653b8dc4
IAyub1 <ismaelayub786@hotmail.com>
2025-04-03 17:45:18 +0100

    .

--- index.js
+++ index.js
@@ -11,7 +11,17 @@ document.getElementById("forecast-button").addEventListener("click", async () =>
 
         const weatherData = {
             day1: new Date(1000 * data.list[0].dt).toLocaleDateString('en-us', { weekday: 'short' }),
-            day1Weather: data.list[0].temp}
+            day1Weather: data.list[0].temp,
+            day2: new Date(1000 * data.list[1].dt).toLocaleDateString('en-us', { weekday: 'short' }),
+            day2Weather: data.list[1].temp,
+            day3: new Date(1000 * data.list[2].dt).toLocaleDateString('en-us', { weekday: 'short' }),
+            day3Weather: data.list[2].temp,
+            day4: new Date(1000 * data.list[3].dt).toLocaleDateString('en-us', { weekday: 'short' }),
+            day4Weather: data.list[3].temp,
+            day5: new Date(1000 * data.list[4].dt).toLocaleDateString('en-us', { weekday: 'short' }),
+            day5Weather: data.list[4].temp
+        }
+
     
     localStorage.setItem("weatherData", JSON.stringify(weatherData));
     window.location.href = "forecast.html";
