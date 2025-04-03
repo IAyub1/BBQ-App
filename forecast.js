856a27d9f87f0c492718d1ff00f499036299c314
IAyub1 <ismaelayub786@hotmail.com>
2025-04-03 19:14:14 +0100

    replaced symbols

--- forecast.js
+++ forecast.js
@@ -4,25 +4,24 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById("weather").innerHTML = "No weather data available.";
         return;
     }
-
     document.getElementById("day1").innerHTML = `
         <h3>${weatherData.day1}</h3>
-        <p>${weatherData.day1Weather.day}°C</p>
+        <p>${weatherData.day1Weather.day > 20 ? '🔥' : '❌'}</p>
     `;
     document.getElementById("day2").innerHTML = `
         <h3>${weatherData.day2}</h3>
-        <p>${weatherData.day2Weather.day}°C</p>
+        <p>${weatherData.day2Weather.day > 20 ? '🔥' : '❌'}</p>
     `;
     document.getElementById("day3").innerHTML = `
         <h3>${weatherData.day3}</h3>
-        <p>${weatherData.day3Weather.day}°C</p>
+        <p>${weatherData.day3Weather.day > 20 ? '🔥' : '❌'}</p>
     `;
     document.getElementById("day4").innerHTML = `
         <h3>${weatherData.day4}</h3>
-        <p>${weatherData.day4Weather.day}°C</p>
+        <p>${weatherData.day4Weather.day > 20 ? '🔥' : '❌'}</p>
     `;
     document.getElementById("day5").innerHTML = `
         <h3>${weatherData.day5}</h3>
-        <p>${weatherData.day5Weather.day}°C</p>
+        <p>${weatherData.day5Weather.day > 20 ? '🔥' : '❌'}</p>
     `;
 });
